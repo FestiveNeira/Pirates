6c6692f00e7f96f2e1b2354e5d23b1855cab744a
lmueller47 <92188368+lmueller47@users.noreply.github.com>
2022-12-02 03:02:30 -0600

    Health and Damage
    works with on trigger enter rn and uses the player to make itself to take damage to allow for multiplayer (this can be adjusted per enemy type).

---
+++ 272 Pirates Game/Assets/Scripts/HealthPlayer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPlayer : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            TakeDamage(20);
+        }
+    }
+}
