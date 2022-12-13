8af3243258e99914c6321c07805223fe16ac294f
lmueller47 <92188368+lmueller47@users.noreply.github.com>
2022-12-13 17:10:04 -0600

    two cutscenes

---
+++ 272 Pirates Game/Assets/Scripts/TypeWriterNewCutscene.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TypeWriterNewCutscene : MonoBehaviour
+{
+    public TMP_Text textLabel;
+
+    private string cutsceneText1 = "I have an Idea!\n";
+    private string cutsceneText2 = "It's Pirate Time!";
+
+    [SerializeField]
+    private float typeWriterSpeed = 50f;
+
+    private void Start()
+    {
+        Run(cutsceneText1, textLabel);
+    }
+
+    public void Run(string textToType, TMP_Text textLabel)
+    {
+        StartCoroutine(TypeText(textToType, textLabel));
+    }
+
+    private IEnumerator TypeText(string textToType, TMP_Text textLabel)
+    {
+        yield return new WaitForSeconds(2f);
+
+        float t = 0;
+        int charIndex = 0;
+
+        while (charIndex < textToType.Length)
+        {
+            t += Time.deltaTime * typeWriterSpeed;
+            charIndex = Mathf.FloorToInt(t);
+            charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
+
+            textLabel.text = textToType.Substring(0, charIndex);
+
+            yield return null;
+        }
+
+        textLabel.text = textToType;
+        Run2(cutsceneText2, textLabel);
+    }
+
+    public void Run2(string textToType, TMP_Text textLabel)
+    {
+        StartCoroutine(TypeText2(textToType, textLabel));
+    }
+
+    private IEnumerator TypeText2(string textToType, TMP_Text textLabel)
+    {
+        yield return new WaitForSeconds(2f);
+
+        float t = 0;
+        int charIndex = 0;
+
+        while (charIndex < textToType.Length)
+        {
+            t += Time.deltaTime * typeWriterSpeed;
+            charIndex = Mathf.FloorToInt(t);
+            charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
+
+            textLabel.text = textToType.Substring(0, charIndex);
+
+            yield return null;
+        }
+
+        textLabel.text = textToType;
+
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene("Supermarket Level");
+    }
+}
